2e6d41e661f532ebc4a0e29270652a12939d490c
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-10-07 12:53:27 +0900

    リレーション課題修正

--- resources/views/recipis/create.blade.php
+++ resources/views/recipis/create.blade.php
@@ -8,50 +8,49 @@
                 <b>{{ count($errors) }}件のエラーがあります。</b>
             </p>
             <ul>
-                @foreach($errors->all() as $error)
+                @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
             </ul>
         </div>
     @endif
 
-<h1>新規レシピ投稿</h1>
+    <h1>新規レシピ投稿</h1>
     <form action="/recipis" method="POST">
-    @csrf
-    <p>
-        <label for="name">料理名</label>
-        <input type="text" name="name" value="{{ old('name') }}">
-    </p>
-    <p>
-        <label for="category_id">カテゴリー</label>
-        <select name="category_id" id="category_id">
-            @foreach ($categories as $category)
-            <option value="{{ $category->id }}" @if (old('category')==$category->id) selected @endif>{{ $category->name }}</option>
-            @endforeach
-        </select>
-    </p>
-    <p>
-        <label for="meterial">材料</label>
-        <textarea name="meterial" id="meterial" cols="30" rows="2">{{ old('meterial') }}</textarea>
-    </p>
-    <p>
-        <label for="seasoning">調味料</label>
-        <textarea name="seasoning" id="seasoning" cols="30" rows="2">{{ old('seasoning') }}</textarea>
-    </p>
-    <p>
-        <label for="recipi">レシピ</label>
-        <textarea name="recipi" id="recipi" cols="30" rows="4">{{ old('recipi') }}</textarea>
-    </p>
-    <p>
-        <label for="img_path">料理画像</label>
-        <input type="text" name="img_path" value="{{ old('img_path') }}">
-    </p>
-    <p>
-        <label for="comment">コメント</label>
-        <textarea name="comment" id="comment" cols="30" rows="2">{{ old('comment') }}</textarea>
-    </p>
-    <input type="submit" value="登録" class="btn btn-outline-info">
-</form>
-<button type="button" onclick="location.href='/recipis'" class="btn btn-outline-success">戻る</button>
+        @csrf
+        <p>
+            <label for="name">料理名</label>
+            <input type="text" name="name" value="{{ old('name') }}">
+        </p>
+        <p>
+            <label for="category_id">カテゴリー</label>
+            <select name="category_id" id="category_id">
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}" @if (old('category') == $category->id) selected @endif>{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </p>
+        <p>
+            <label for="meterial">材料</label>
+            <textarea name="meterial" id="meterial" cols="30" rows="2">{{ old('meterial') }}</textarea>
+        </p>
+        <p>
+            <label for="seasoning">調味料</label>
+            <textarea name="seasoning" id="seasoning" cols="30" rows="2">{{ old('seasoning') }}</textarea>
+        </p>
+        <p>
+            <label for="recipi">レシピ</label>
+            <textarea name="recipi" id="recipi" cols="30" rows="4">{{ old('recipi') }}</textarea>
+        </p>
+        <p>
+            <label for="img_path">料理画像</label>
+            <input type="text" name="img_path" value="{{ old('img_path') }}">
+        </p>
+        <p>
+            <label for="comment">コメント</label>
+            <textarea name="comment" id="comment" cols="30" rows="2">{{ old('comment') }}</textarea>
+        </p>
+        <input type="submit" value="登録" class="btn btn-outline-info">
+    </form>
+    <button type="button" onclick="location.href='/recipis'" class="btn btn-outline-success">戻る</button>
 @endsection
-
